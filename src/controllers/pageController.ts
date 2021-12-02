3a33458837640fbd5f3b429390a2d65030ae6d50
João Gustavo Ribeiro da Silva <joaogustavosilva2001@gmail.com>
2021-12-02 16:05:56 -0300

    :sparkles: Added content to pages

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,34 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page')
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos animais',
+            background: 'allanimals.jpg'
+        }
+    })
 }
 export const dog = (req: Request, res: Response) => {
-
+    res.render('pages/page', {
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    })
 }
 export const cat = (req: Request, res: Response) => {
-
+    res.render('pages/page', {
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
 }
 export const fish = (req: Request, res: Response) => {
-
+    res.render('pages/page', {
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
 }
